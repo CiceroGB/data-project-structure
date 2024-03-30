a00cca4f59fac3dab2b07d4ac2b4a234ab4021c1
CiceroGB <brunelli.cg@gmail.com>
2024-03-30 12:09:10 -0300

    chore: load

--- app/etl/load.py
+++ app/etl/load.py
@@ -3,24 +3,19 @@ import pandas as pd
 
 def load_em_um_novo_excel(df: pd.DataFrame, output_folder: str, output_file_name: str) -> None:
     """
-    Salva um DataFrame do pandas em um arquivo Excel em um diretório especificado.
-    
+    Salva um DataFrame em um arquivo Excel no diretório especificado.
+
     Args:
-        df (pd.DataFrame): DataFrame que será salvo em um arquivo Excel.
-        output_folder (str): Caminho do diretório onde o arquivo Excel será salvo.
+        df (pd.DataFrame): DataFrame a ser salvo em arquivo Excel.
+        output_folder (str): Caminho da pasta onde o arquivo Excel será salvo.
         output_file_name (str): Nome do arquivo Excel a ser criado.
-    
-    Raises:
-        PermissionError: Se ocorrer um erro de permissão ao salvar o arquivo Excel.
-        Exception: Se ocorrer um erro ao salvar o arquivo Excel.
+
+    Não retorna nada, mas salva o arquivo Excel no caminho especificado.
     """
+    # Verifica se a pasta de saída existe, se não, cria a pasta
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    try:
-        file_path = os.path.join(output_folder, output_file_name)
-        df.to_excel(file_path, index=False)
-    except PermissionError as e:
-        raise PermissionError(f"Erro de permissão ao salvar o arquivo Excel: {e}")
-    except Exception as e:
-        raise Exception(f"Erro ao salvar o arquivo Excel: {e}")
+    # Salva o DataFrame em um arquivo Excel no caminho especificado
+    file_path = os.path.join(output_folder, output_file_name)
+    df.to_excel(file_path, index=False)  # O parâmetro index=False remove o índice do DataFrame no arquivo Excel
