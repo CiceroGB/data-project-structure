188ea8d27b92abc0044cae674b8843ce11204303
CiceroGB <brunelli.cg@gmail.com>
2024-03-30 12:09:34 -0300

    chore: transform

--- app/etl/transform.py
+++ app/etl/transform.py
@@ -1,18 +1,16 @@
 from typing import List
 import pandas as pd
+from pandas import DataFrame
 
-def transforma_em_um_unico(all_data: List[pd.DataFrame]) -> pd.DataFrame:
+def transforma_em_um_unico(all_data: List[DataFrame]) -> DataFrame:
     """
-    Consolida uma lista de DataFrames do pandas em um único DataFrame.
-
-    Esta função concatena verticalmente todos os DataFrames fornecidos na lista `all_data`.
-    O índice do DataFrame resultante é redefinido para garantir a unicidade.
+    Consolida uma lista de DataFrames em um único DataFrame.
 
     Args:
-        all_data (List[pd.DataFrame]): Lista de DataFrames do pandas para consolidar.
+        all_data (List[DataFrame]): Lista de DataFrames para serem consolidados.
 
     Returns:
-        pd.DataFrame: DataFrame único consolidado de todos os DataFrames na lista.
+        DataFrame: DataFrame consolidado contendo todos os dados dos DataFrames da lista.
 
     Raises:
         ValueError: Se a lista `all_data` estiver vazia ou contiver elementos que não são DataFrames.
@@ -20,11 +18,8 @@ def transforma_em_um_unico(all_data: List[pd.DataFrame]) -> pd.DataFrame:
     if not all_data:
         raise ValueError("No data to transform")
 
-    # Verifica se todos os elementos na lista são DataFrames
-    if not all(isinstance(df, pd.DataFrame) for df in all_data):
+    if not all(isinstance(item, DataFrame) for item in all_data):
         raise ValueError("All elements in the list must be pandas DataFrames")
 
-    # Concatena todos os DataFrames na lista em um único DataFrame
     consolidated_df = pd.concat(all_data, axis=0, ignore_index=True)
-
     return consolidated_df
